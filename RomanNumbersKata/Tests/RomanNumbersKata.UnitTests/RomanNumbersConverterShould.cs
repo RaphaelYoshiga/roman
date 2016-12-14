809059fb3b4712413443d0de96000b6e78c17849
Jonathan Povey <jonathan.povey@asos.com>
2016-12-14 10:20:44 +0000

    ReturnI_WhenConverting_GivenInputIs1

---
+++ RomanNumbersKata/Tests/RomanNumbersKata.UnitTests/RomanNumbersConverterShould.cs
@@ -0,0 +1,33 @@
+﻿
+namespace RomanNumbersKata.UnitTests
+{
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RomanNumbersConverterShould
+    {
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        public void ReturnI_WhenConverting_GivenInputIs1(int number, string expectedRoman)
+        {
+            var roman = RomanNumbersConverter.Convert(number);
+
+            roman.Should().Be(expectedRoman);
+        }        
+    }
+
+    public class RomanNumbersConverter
+    {
+        public static string Convert(int number)
+        {
+            if (number == 2)
+            {
+                return "II";
+            }
+
+            return "I";
+        }
+    }
+}
