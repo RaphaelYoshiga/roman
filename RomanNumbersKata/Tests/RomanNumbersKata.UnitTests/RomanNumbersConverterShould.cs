5ce448576a54ca1571247c24bad056e8aff150bd
Jonathan Povey <jonathan.povey@asos.com>
2016-12-14 10:37:36 +0000

    ReturnRoman_WhenConverting_GivenInputIsGreaterThanThree

--- RomanNumbersKata/Tests/RomanNumbersKata.UnitTests/RomanNumbersConverterShould.cs
+++ RomanNumbersKata/Tests/RomanNumbersKata.UnitTests/RomanNumbersConverterShould.cs
@@ -17,12 +17,37 @@ namespace RomanNumbersKata.UnitTests
 
             roman.Should().Be(expectedRoman);
         }
+
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        public void ReturnRoman_WhenConverting_GivenInputIsGreaterThanThree(int number, string expectedRoman)
+        {
+            var roman = RomanNumbersConverter.Convert(number);
+
+            roman.Should().Be(expectedRoman);
+        }
     }
 
     public class RomanNumbersConverter
     {
         public static string Convert(int number)
         {
+            if (number == 4)
+            {
+                return "IV";
+            }
+
+            if (number == 5)
+            {
+                return "V";
+            }
+
+            if (number == 6)
+            {
+                return "VI";
+            }
+
             var index = number - 1;
 
             var romanNumbers = new[] { "I", "II", "III" };
